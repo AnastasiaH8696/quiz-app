f338c468985f4c7ed5286a96c66c3c2472fee186
Anastasia Hamandritov <53013989+AnastasiaH8696@users.noreply.github.com>
2023-08-26 13:22:51 +0300

    Adding QuizQuestion logic

--- src/main/java/il/ac/hit/quizzy/QuizQuestion.java
+++ src/main/java/il/ac/hit/quizzy/QuizQuestion.java
@@ -3,28 +3,79 @@ package il.ac.hit.quizzy;
 import il.ac.hit.quizzy.interfaces.IQuizQuestion;
 import il.ac.hit.quizzy.interfaces.IQuizQuestionBuilder;
 
-public class QuizQuestion implements IQuizQuestion {
-    public static class Builder implements IQuizQuestionBuilder {
+import java.util.ArrayList;
+import java.util.List;
 
-        //Testing commit
+public class QuizQuestion implements IQuizQuestion {
+
+    private String title;
+    private String question;
+    private List<Answer> answers;
+
+    private QuizQuestion() {
+        answers = new ArrayList<>();
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    @Override
+    public void addAnswer(String text, boolean correct) {
+        answers.add(new Answer(text, correct));
+    }
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public boolean isAnswerCorrect(int answerIndex) {
+        return this.answers.get(answerIndex).correct;
+    }
+
+    public static class Builder implements IQuizQuestionBuilder {
+        private QuizQuestion quizQuestion;
+
+        public Builder() {
+            quizQuestion = new QuizQuestion();
+        }
+
         @Override
         public IQuizQuestionBuilder setTitle(String text) {
-            return null;
+            quizQuestion.setTitle(text);
+            return this;
         }
 
         @Override
         public IQuizQuestionBuilder setQuestion(String text) {
-            return null;
+            quizQuestion.setQuestion(text);
+            return this;
         }
 
         @Override
         public IQuizQuestionBuilder addAnswer(String text, boolean correct) {
-            return null;
+            quizQuestion.addAnswer(text, correct);
+            return this;
         }
 
         @Override
         public IQuizQuestion create() {
-            return null;
+            return quizQuestion;
+        }
+    }
+
+    private static class Answer {
+        String text;
+        boolean correct;
+
+        Answer(String text, boolean correct) {
+            this.text = text;
+            this.correct = correct;
         }
     }
 }
