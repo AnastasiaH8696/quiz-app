d6ed42923436d58b09eff3babd4162595ba332f7
Anastasia Hamandritov <53013989+AnastasiaH8696@users.noreply.github.com>
2023-08-26 13:23:17 +0300

    Update IQuizQuestion.java with functions

--- src/main/java/il/ac/hit/quizzy/interfaces/IQuizQuestion.java
+++ src/main/java/il/ac/hit/quizzy/interfaces/IQuizQuestion.java
@@ -1,4 +1,7 @@
 package il.ac.hit.quizzy.interfaces;
 
 public interface IQuizQuestion {
+    void setTitle(String title);
+    void setQuestion(String question);
+    void addAnswer(String text, boolean correct);
 }
