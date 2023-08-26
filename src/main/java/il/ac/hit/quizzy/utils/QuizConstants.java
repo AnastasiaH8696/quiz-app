22d6373e125dbe700f311a0380b9736299070c6c
Anastasia Hamandritov <53013989+AnastasiaH8696@users.noreply.github.com>
2023-08-26 17:00:30 +0300

    Adding utils package and quiz constants

---
+++ src/main/java/il/ac/hit/quizzy/utils/QuizConstants.java
@@ -0,0 +1,8 @@
+package il.ac.hit.quizzy.utils;
+public final class QuizConstants {
+
+    private QuizConstants() {
+        // No need to instantiate the class, we can hide its constructor
+    }
+    public static final int POINTS_TO_ADD = 5;
+}
