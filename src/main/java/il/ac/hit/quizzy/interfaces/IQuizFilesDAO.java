15327113655e801f4b83b6cc1bfce27deae65354
Anastasia Hamandritov <53013989+AnastasiaH8696@users.noreply.github.com>
2023-09-03 15:03:17 +0300

    Update IQuizFilesDAO.java

--- src/main/java/il/ac/hit/quizzy/interfaces/IQuizFilesDAO.java
+++ src/main/java/il/ac/hit/quizzy/interfaces/IQuizFilesDAO.java
@@ -5,6 +5,4 @@ import il.ac.hit.quizzy.exceptions.QuizException;
 public interface IQuizFilesDAO {
     public abstract void saveQuizToFile(IQuiz quiz, String fileName) throws QuizException;
     public abstract IQuiz loadQuizFromFile(String fileName) throws QuizException;
-
-    void saveToFile(IQuiz quiz, String s);
 }
